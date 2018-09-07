9d86035849de90a63a77c28d5f5893f79a6e3bcf
kleberdiazc <kleberstevn-diaz@hotmail.com>
2018-09-07 10:24:05 -0500

    me falto
    falta eso

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const http = require('http'),
       bodyParser = require('body-parser'),
       express = require('express'),
       routes = require('./routes');
+      path = require('path');
 
 const port = process.env.PORT ||  3000,
       app = express(),
@@ -11,9 +12,10 @@ routes(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'../public')));
 // app.use('/search', routes);
 
 server.listen(port, () => {
     console.log("Listening on port " + port);
+    console.log()
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,10 @@ const storage = require('../storage'),
       path = require('path');
 
 const routes = (app) => {
+
+    app.get('/',(req,res)=>{
+      res.sendFile(path.join(__dirname,'../'+'../public/index.html'));
+    });
     app.get('/search', (req, res) => {
         storage.getDataAll()
             .then((data) => {
