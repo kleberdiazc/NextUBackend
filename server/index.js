7d89641f94526b63410b5af4966a63868845e780
kleberdiazc <kleberstevn-diaz@hotmail.com>
2018-09-07 09:21:46 -0500

    FIN
    terminado

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-const http= require('http'),
+const http = require('http'),
       bodyParser = require('body-parser'),
       express = require('express'),
       routes = require('./routes');
 
+const port = process.env.PORT ||  3000,
+      app = express(),
+      server = http.createServer(app);
 
-const port = process.env.PORT || 3000,
-        app = express(),
-        server = http.createServer(app);
 routes(app);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+// app.use('/search', routes);
 
-
-server.listen(port,() => {
-  console.log("Listening")
+server.listen(port, () => {
+    console.log("Listening on port " + port);
 });
